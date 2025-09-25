b746e540e13b5c3f00732a60ac73b4bce5839b50
HuangShaoFu111 <jason.930701@gmail.com>
2025-09-25 15:19:28 +0800

    modified:111303307_js_2.js/新增input "INPUT_ID"

--- HW4_JavaScript1/static/111303307_js_2.js
+++ HW4_JavaScript1/static/111303307_js_2.js
@@ -1,3 +1,5 @@
+const INPUT_ID = "exprInput";
+
 document.write('<div style="display:grid; grid-template-columns: repeat(3, 1fr); gap:6px; margin-bottom:8px;">');
 for (let i = 0; i <= 9; i++) {
   document.write(`<button style="padding:8px; font-size:18px;" onclick="appAppend('${i}')">${i}</button>`);
