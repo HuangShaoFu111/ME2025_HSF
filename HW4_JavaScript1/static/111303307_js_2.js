a6619567eb2e1f1063ac0b59c46a9f033d64168d
HuangShaoFu111 <jason.930701@gmail.com>
2025-09-25 15:23:21 +0800

    modified:111303307_js_2.js/ 宣告功能(輸入、清空、計算)

--- HW4_JavaScript1/static/111303307_js_2.js
+++ HW4_JavaScript1/static/111303307_js_2.js
@@ -1,5 +1,20 @@
 const INPUT_ID = "exprInput";
 
+// 先宣告功能函式，給onclick
+window.appAppend = function (ch) {
+  const ipt = document.getElementById(INPUT_ID);
+  ipt.value += ch;
+  ipt.focus();
+};
+window.appClear = function () {
+  const ipt = document.getElementById(INPUT_ID);
+  ipt.value = "";
+  ipt.focus();
+};
+window.appEval = function () {
+  const ipt = document.getElementById(INPUT_ID);
+  const expr = ipt.value;
+  
 document.write('<div style="display:grid; grid-template-columns: repeat(3, 1fr); gap:6px; margin-bottom:8px;">');
 for (let i = 0; i <= 9; i++) {
   document.write(`<button style="padding:8px; font-size:18px;" onclick="appAppend('${i}')">${i}</button>`);
