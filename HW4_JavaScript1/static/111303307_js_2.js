828a61952d3800a53e16d0b1e2560d97a4a72457
HuangShaoFu111 <jason.930701@gmail.com>
2025-09-25 15:40:23 +0800

    mpdified:111303307_js_2.js/ 新增按鈕(符號、claer)並指定相對應function、新增輸入框、指定畫面排版

--- HW4_JavaScript1/static/111303307_js_2.js
+++ HW4_JavaScript1/static/111303307_js_2.js
@@ -27,9 +27,33 @@ window.appEval = function () {
     ipt.focus();
   };
 
+// 指定畫面排版 
+document.write('<div style="max-width: 320px; font-family: system-ui, -apple-system, Segoe UI, Roboto, Noto Sans, Arial; line-height:1.4;">');
 
+// 輸入框
+document.write(`<input id="${INPUT_ID}" type="text" style="width:100%; padding:8px; font-size:18px; box-sizing:border-box; margin-bottom:12px;" placeholder="請輸入算式，例如：6*(8+8)"/>`);
+
+//繪出數字鍵盤
 document.write('<div style="display:grid; grid-template-columns: repeat(3, 1fr); gap:6px; margin-bottom:8px;">');
 for (let i = 0; i <= 9; i++) {
   document.write(`<button style="padding:8px; font-size:18px;" onclick="appAppend('${i}')">${i}</button>`);
 }
+document.write('</div>');
+
+// clear
+document.write('<div style="margin-bottom:8px;">');
+document.write('<button style="padding:8px; font-size:18px; width:100%;" onclick="appClear()">clear</button>');
+document.write('</div>');
+
+// 符號列：「+ - * / ( ) =」
+const ops = ["+", "-", "*", "/", "(", ")", "="];
+document.write('<div style="display:grid; grid-template-columns: repeat(7, 1fr); gap:6px;">');
+ops.forEach(op => {
+  if (op === "=") {
+    document.write(`<button style="padding:8px; font-size:18px;" onclick="appEval()">${op}</button>`);
+  } else {
+    document.write(`<button style="padding:8px; font-size:18px;" onclick="appAppend('${op}')">${op}</button>`);
+  }
+});
+document.write('</div>');
 document.write('</div>');
