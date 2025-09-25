37cb2afb5c979fc3a93b25ffa7264c88c3c9ebca
HuangShaoFu111 <jason.930701@gmail.com>
2025-09-25 15:26:02 +0800

    modified:111303307_js_2.js/完整eval功能

--- HW4_JavaScript1/static/111303307_js_2.js
+++ HW4_JavaScript1/static/111303307_js_2.js
@@ -6,15 +6,28 @@ window.appAppend = function (ch) {
   ipt.value += ch;
   ipt.focus();
 };
+
 window.appClear = function () {
   const ipt = document.getElementById(INPUT_ID);
   ipt.value = "";
   ipt.focus();
 };
+
 window.appEval = function () {
   const ipt = document.getElementById(INPUT_ID);
   const expr = ipt.value;
-  
+  try {
+      // 使用 eval 計算
+      const ans = eval(expr);
+      alert(`${expr} = ${ans}`);
+      ipt.value = ans; // 輸入框改為答案
+    } catch (err) {
+      alert("表達式錯誤，請重新輸入。");
+    }
+    ipt.focus();
+  };
+
+
 document.write('<div style="display:grid; grid-template-columns: repeat(3, 1fr); gap:6px; margin-bottom:8px;">');
 for (let i = 0; i <= 9; i++) {
   document.write(`<button style="padding:8px; font-size:18px;" onclick="appAppend('${i}')">${i}</button>`);
